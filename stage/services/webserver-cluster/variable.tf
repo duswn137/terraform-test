9d3288d91f6a6e36338c5ce1a3f66c115338b167
duswn137 <yjko9937@gmail.com>
2024-02-01 02:52:54 +0000

    state

---
+++ stage/services/webserver-cluster/variable.tf
@@ -0,0 +1,16 @@
+variable "server_port" {
+  description = "server port for http request"
+  type = number
+  default = 8080
+}
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
